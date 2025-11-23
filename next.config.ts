5b293fd588ab9273c0b31181d13f37ea6b3045ef
Anshu Patel <anshu.beta.r@gmail.com>
2025-11-23 15:33:52 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
